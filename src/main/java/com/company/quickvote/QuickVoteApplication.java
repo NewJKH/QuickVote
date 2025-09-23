265fc9db10fa464674685e165637d5009b61806b
NewJKH <newsun0805@gmail.com>
2025-09-23 12:09:49 +0900

    init

---
+++ src/main/java/com/company/quickvote/QuickVoteApplication.java
@@ -0,0 +1,13 @@
+package com.company.quickvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickVoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/quickvote/QuickVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.quickvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
